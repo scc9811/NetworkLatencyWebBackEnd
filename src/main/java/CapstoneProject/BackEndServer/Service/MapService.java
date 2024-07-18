7c12c244b5609346b5fa74ac703b6efa52c37662
scc9811 <wjsrudtjr98@naver.com>
2024-07-18 17:16:20 +0900

    ip 위치정보 API 변경
    이번엔 Server 위치가 정확하지 않아서 변경
    기존 : ip2location.io
    변경 : ipregistry.co

--- src/main/java/CapstoneProject/BackEndServer/Service/MapService.java
+++ src/main/java/CapstoneProject/BackEndServer/Service/MapService.java
@@ -1,6 +1,7 @@
 package CapstoneProject.BackEndServer.Service;
 
 import CapstoneProject.BackEndServer.Dto.GeoLocationData;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 @Service
+@Slf4j
 public class MapService {
 
     @Value("${ipInfo.key}")
@@ -18,7 +20,8 @@ public class MapService {
 
     public GeoLocationData getLocation(String ip) {
 //        String url = "https://ipinfo.io/" + ip + "?token=" + token;
-        String url = "https://api.ip2location.io/?key=" + token + "&ip=" + ip;
+//        String url = "https://api.ip2location.io/?key=" + token + "&ip=" + ip;
+        String url = "https://api.ipregistry.co/" + ip + "?key=" + token;
         try{
             URL obj = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
@@ -33,6 +36,7 @@ public class MapService {
 
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
+                log.info(inputLine);
             }
 
             in.close();
@@ -40,6 +44,24 @@ public class MapService {
             // JSON 응답 처리
             String jsonResponse = response.toString();
             JSONObject jsonObject = new JSONObject(jsonResponse);
+
+            // response 형태 :
+            // {
+            //	"location": {
+            //		"region": {
+            //			"latitude": 37.7749,
+            //			"longitude": -122.4194
+            //		}
+            //	}
+            //}
+
+            // location 객체
+            JSONObject location = jsonObject.getJSONObject("location");
+
+            // region 객체
+            JSONObject region = location.getJSONObject("region");
+
+
 
             // "loc" 키로부터 위도와 경도 추출
 //            String loc = jsonObject.getString("loc");
@@ -49,9 +71,11 @@ public class MapService {
 //            geoLocationData.setLatitude(latLong[0]);
 //            geoLocationData.setLongitude(latLong[1]);
 
+            geoLocationData.setLatitude(String.valueOf(region.getDouble("latitude")));
+            geoLocationData.setLongitude(String.valueOf(region.getDouble("longitude")));
 
-            geoLocationData.setLatitude(jsonObject.getString("latitude"));
-            geoLocationData.setLongitude(jsonObject.getString("longitude"));
+            log.info(String.valueOf(region.getDouble("latitude")));
+            log.info(String.valueOf(region.getDouble("longitude")));
 
             return geoLocationData;
         }
--- src/test/java/CapstoneProject/BackEndServer/Service/MapServiceTest.java
+++ src/test/java/CapstoneProject/BackEndServer/Service/MapServiceTest.java
@@ -4,21 +4,9 @@ import CapstoneProject.BackEndServer.Dto.GeoLocationData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.BufferedReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,7 +23,7 @@ class MapServiceTest {
     }
 
     @Test
-    @DisplayName("ip 기반 위치 테스트")
+    @DisplayName("ip 기반 위치정보 추출 테스트")
     void getLocation(){
         // server IP
         String ip = "54.180.58.154";
@@ -44,7 +32,7 @@ class MapServiceTest {
         GeoLocationData result = mapService.getLocation(ip);
 
         assertNotNull(result);
-        assertEquals("37.4565", result.getLatitude());
+        assertEquals("37.456", result.getLatitude());
         assertEquals("126.7052", result.getLongitude());
 
 
